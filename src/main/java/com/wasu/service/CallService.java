d6cbedabfed58ad332e36866cff6c4bba3436ea7
xv330790416 <5287xv123456>
2017-03-14 16:02:03 +0800

    alarmSum

--- src/main/java/com/wasu/service/CallService.java
+++ src/main/java/com/wasu/service/CallService.java
@@ -2,7 +2,6 @@ package com.wasu.service;
 
 
 import com.wasu.model.AlarmDO;
-import com.wasu.model.Call;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ import java.util.List;
  * Created by kale on 2016/12/19.
  */
 public interface CallService {
-    public List<Call> findCall();
 
     public List<AlarmDO> queryAlarmNum(String cityName);
 }
--- src/main/java/com/wasu/service/impl/CallServiceImpl.java
+++ src/main/java/com/wasu/service/impl/CallServiceImpl.java
@@ -1,11 +1,8 @@
 package com.wasu.service.impl;
 
 import com.wasu.dao.AlarmDOMapper;
-import com.wasu.dao.CallMapper;
 import com.wasu.model.AlarmDO;
 import com.wasu.model.AlarmDOExample;
-import com.wasu.model.Call;
-import com.wasu.model.CallExample;
 import com.wasu.service.CallService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +19,10 @@ public class CallServiceImpl implements CallService{
 
     private static Logger logger = Logger.getLogger(CallServiceImpl.class.getName());
 
-    @Resource
-    private CallMapper callMapper;
 
     @Resource
     private AlarmDOMapper alarmDOMapper;
 
-    public List<Call> findCall() {
-        CallExample callExample=new CallExample();
-        CallExample.Criteria criteria=callExample.createCriteria();
-//        criteria.andIdEqualTo(2L);
-        return callMapper.selectByExample(callExample);
-    }
 
     public List<AlarmDO> queryAlarmNum(String cityName) {
         AlarmDOExample alarmDOExample=new AlarmDOExample();
